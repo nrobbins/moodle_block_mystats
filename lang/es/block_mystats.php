a3a20921e9f7667dfff6c41e93b4367fefafaceb
Nathan Robbins <nirobbins@navitor.com>
2012-10-28 16:03:18 -0400

    Initial Spanish language strings (need review)
    Corrections are welcome

---
+++ lang/es/block_mystats.php
@@ -0,0 +1,104 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'block_mystats'
+ * Language = es
+ * @package   block_mystats
+ * @copyright 2012 Nathan Robbins (https://github.com/nrobbins)
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'My Stats';
+$string['blocktitle'] = 'Título del bloque';
+$string['blocktitle_help'] = 'El título de este instancia del bloque';
+//Setting Strings
+$string['blocksettings'] = 'Configuración del bloque My Stats';
+$string['adminheading'] = 'Configuración Global';
+$string['admintext'] = 'Esta configuración afecta a todas las instancias del bloque My Stats block.';
+$string['allowuserconfig'] = 'Permitir configuración de usuario';
+$string['allowuserconfigtext'] = 'Permitir a los usuarios elegir qué secciones se muestran (de las permitidas en esta configuración)';
+$string['allowprofile'] = 'Permitir en las páginas de perfil';
+$string['allowprofiletext'] = 'Permita que el bloque Mi Estadísticas de aparecer en las páginas de perfil de usuario. Estadísticas relacionadas con los grados están ocultas en este contexto.';
+$string['showcharts'] = 'Mostrar gráficos';
+$string['showchartstext'] = 'Mostrar gráficos para cada grupo de estadísticas. Cuando está desactivado, se presenta la información como texto.';
+$string['showforum'] = 'Foros';
+$string['showforumtext'] = 'Mostrar estadísticas foro';
+$string['showblog'] = 'Blogs';
+$string['showblogtext'] = 'Mostrar estadísticas blog';
+$string['showquiz'] = 'Cuestionarios';
+$string['showquiztext'] = 'Mostrar estadísticas cuestionario';
+$string['showlesson'] = 'Lecciones';
+$string['showlessontext'] = 'Mostrar estadísticas lección';
+$string['showassignment'] = 'Tareas';
+$string['showassignmenttext'] = 'Mostrar estadísticas tarea';
+$string['showmsg'] = 'Mensajes';
+$string['showmsgtext'] = 'Mostrar estadísticas mensaje';
+$string['showfile'] = 'Archivos';
+$string['showfiletext'] = 'Mostrar estadísticas de archivos';
+$string['showglossary'] = 'Entradas de glosario';
+$string['showglossarytext'] = 'Mostrar estadísticas para las entradas del glosario';
+//Forum Strings
+$string['forums'] = 'Foros';
+$string['forumposts'] = 'Todos mis mensajes en los foros';
+$string['forumtopics'] = 'Temas nuevos';
+$string['forumreplies'] = 'Respuestas';
+$string['noposts'] = 'No hay mensajes';
+$string['latestpost'] = 'Último mensaje';
+//Blog Strings
+$string['blogs'] = 'Blogs';
+$string['blogposts'] = 'Todos los posts de blog';
+$string['blogcourse'] = 'Relacionados con un curso';
+$string['blogactivity'] = 'Relacionado con una actividad';
+//Quiz Strings
+$string['quizzes'] = 'Cuestionarios';
+$string['quizattempt'] = 'Intentos de cuestionarios';
+$string['quizavgscore'] = 'Puntaje promedio';
+$string['quizhighscore'] = 'puntaje más alto';
+$string['quizscores'] = 'Resultados cuestionario';
+$string['lastattempt'] = 'Último intento';
+$string['noattemptsrecorded'] = 'No hay intentos';
+//Lesson Strings
+$string['lessons'] = 'Lecciones';
+$string['lessonattempt'] = 'Intentos de lecciones';
+$string['lessonscores'] = 'Resultados lección';
+//Assignment Strings
+$string['assignments'] = 'Tareas';
+$string['assignmentattempt'] = 'Entregas de tareas';
+$string['assignmentscores'] = 'Resultados de tareas';
+//Message Strings
+$string['messages'] = 'Mensajes';
+$string['messagesent'] = 'Mensajes enviados';
+$string['messagereceived'] = 'Mensajes recibidos';
+$string['messagecontacts'] = 'Contactos';
+//File Strings
+$string['files'] = 'Archivos';
+$string['totalfiles'] = 'Todos mis archivos';
+$string['fileprivate'] = 'Archivos privados';
+$string['fileattached'] = 'Adjuntos a mensajes';
+$string['filesubmitted'] = 'Archivos enviados para tareas';
+$string['filetotalsize'] = 'Tamaño total de archivos cargados';
+$string['fileavgsize'] = 'Tamaño promedio del archivos';
+$string['largestfile'] = 'Archivo más grande';
+$string['bytes'] = 'bytes';
+//Glossary Strings
+$string['glossary'] = 'Glosario';
+$string['glossaryentries'] = 'Entradas de glosario';
+$string['glossaryapproved'] = 'Entradas aprobadas';
+//Other Strings
+$string['scorepercent'] = 'Puntos Porcentaje';
+$string['nogrades'] = 'No hay calificaciones registradas';
